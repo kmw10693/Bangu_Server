f9c53478a786d2ceb0cec26edbffd13d529c4cbd
minu kim <kmw106933@naver.com>
2022-04-06 01:28:31 +0900

    [Feat] MovieListRes 추가

---
+++ src/main/java/com/ott/ott_server/dto/movie/MovieListRes.java
@@ -0,0 +1,28 @@
+package com.ott.ott_server.dto.movie;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "회원가입 축하 영화 결과 정보")
+public class MovieListRes {
+
+    @ApiModelProperty(value = "영화 식별자", example = "1")
+    private Long movieId;
+
+    @ApiModelProperty(value = "영화 이미지", example = "image.com")
+    private String imageUrl;
+
+    @ApiModelProperty(value = "영화 평점", example = "4.5")
+    private BigDecimal score;
+
+}
