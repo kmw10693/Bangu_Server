2e1fda62dc89a5af13a0e69ba75d88d522837d9a
minu kim <kmw106933@naver.com>
2022-04-06 01:27:38 +0900

    [Refactor] preAuthorize 적용

--- src/main/java/com/ott/ott_server/controllers/HelloController.java
+++ src/main/java/com/ott/ott_server/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.ott.ott_server.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
+    @PreAuthorize("permitAll()")
     public String sayHello() { return "Hello world!"; }
 }
