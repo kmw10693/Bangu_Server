1c68a1d484b94e7cc3eba3e1915f4c43e53500f3
minu kim <kmw106933@naver.com>
2022-04-06 01:28:45 +0900

    [Feat] 리뷰 평점 계산 쿼리 추가

--- src/main/java/com/ott/ott_server/infra/MovieRepository.java
+++ src/main/java/com/ott/ott_server/infra/MovieRepository.java
@@ -1,13 +1,24 @@
 package com.ott.ott_server.infra;
 
 import com.ott.ott_server.domain.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findAllByTitleContaining(String search);
+
+    // 주어진 영화에 대한 리뷰들의 평점을 계산하여 가져옵니다.
+    @Query("select avg(score) from Review where movie = :movie and deleted = false")
+    BigDecimal calcScoreAvg(Movie movie);
+
+    Page<Movie> findAllByDeletedFalse(Pageable pageable);
+
 }
